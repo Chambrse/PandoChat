583bb501f1ddf5b71232396fb190ef5c80dfb533
Shane Chambry <shaneechambry@gmail.com>
2023-07-19 12:23:09 -0400

    attempt to fix mongo connection string

--- database/index.js
+++ database/index.js
@@ -4,7 +4,8 @@ mongoose.Promise = global.Promise;
 
 // your local database url
 // 27017 is the default mongoDB port
-const uri = process.env.MONGODB_URI || 'mongodb+srv://admin:TzuMExlmksWQgdg7@cluster0.pxi75yz.mongodb.net/?retryWrites=true&w=majority';
+// const uri = process.env.MONGODB_URI || 'mongodb://admin:TzuMExlmksWQgdg7@ac-ru4r8pb-shard-00-00.pxi75yz.mongodb.net:27017,ac-ru4r8pb-shard-00-01.pxi75yz.mongodb.net:27017,ac-ru4r8pb-shard-00-02.pxi75yz.mongodb.net:27017/?ssl=true&replicaSet=atlas-f8wkws-shard-0&authSource=admin&retryWrites=true&w=majority';
+const uri = process.env.MONGODB_URI || 'mongodb+srv://admin:TzuMExlmksWQgdg7@cluster0.pxi75yz.mongodb.net/Pandochat?retryWrites=true&w=majority';
 // const uri = process.env.MONGODB_URI || 'mongodb+srv://apiUser:SXzAbUa2gZ1BgOYw@cluster0.pxi75yz.mongodb.net/?retryWrites=true&w=majority';
 // const uri = "mongodb://localhost:27017/pandochatLocal"
 
